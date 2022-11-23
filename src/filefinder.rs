b4753069cd282aa2833950adab2551c0273506bf
KIwGucci <kiw.a.gucci@gmail.com>
2022-11-24 07:40:32 +0900

    Code refactoring

--- src/filefinder.rs
+++ src/filefinder.rs
@@ -1,54 +1,54 @@
-use globmatch::{self, IterAll};
+extern crate globmatch;
+use anyhow::Result;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-pub fn finder(rootpath: &str, words: &str, extention: &str) -> IterAll<PathBuf> {
+pub fn item_search(rootpath: &str, extention: &str, searchword: &str) -> Vec<PathBuf> {
     // words の　一番目の単語をターゲットワードに設定
-    let target = format!(
-        "./**/*{}*.{}",
-        words.split(" ").collect::<Vec<&str>>()[0],
-        extention
-    );
-    let rootpath = Path::new(rootpath).to_str().unwrap();
+    let target = format!("./**/*.{}", extention);
 
-    let files = globmatch::Builder::new(&target)
+    let rootpath = Path::new(rootpath);
+
+    let builder = globmatch::Builder::new(&target)
         .case_sensitive(false)
         .build(rootpath)
-        .unwrap()
-        .into_iter();
+        .expect("Could not build globmatcher");
 
-    files
+    let paths = builder.into_iter().filter_map(|x| x.ok());
+
+    paths.filter(|f| is_contain(f, searchword)).collect()
 }
 
-pub fn opendir(fpath: &PathBuf) -> Result<(), std::io::Error> {
+pub fn opendir(fpath: &Path) -> Result<(), std::io::Error> {
     // 対象ファイルのフォルダをファインダーで開く
     #[cfg(target_os = "macos")]
     Command::new("open").arg(fpath.parent().unwrap()).spawn()?;
+
     #[cfg(target_os = "windows")]
     Command::new("explorer")
         .arg(fpath.parent().unwrap())
         .spawn()?;
+
     #[cfg(target_os = "linux")]
     Command::new("xdg-open")
         .arg(fpath.parent().unwrap())
         .spawn()?;
+
     Ok(())
 }
 
-pub fn andsearch(path: &PathBuf, words: &str) -> bool {
-    if !words.contains(" ") {
-        return true;
-    }
+fn is_contain(path: &Path, words: &str) -> bool {
+
     for wd in words.split_whitespace() {
         if wd != "*"
             && !(path
-                .to_str()
-                .unwrap()
+                .to_string_lossy()
                 .to_lowercase()
                 .contains(&wd.to_lowercase()))
         {
             return false;
         }
     }
+
     true
 }
--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,17 @@ use std::{
     path::PathBuf,
 };
 
+const DISPLAY_LIMIT:usize = 100;
+
 fn main() -> Result<()> {
     let mut my_readline = Editor::<()>::new()?;
 
     execute!(stdout(), terminal::Clear(terminal::ClearType::All))?;
 
     println!("File Finder.\nInput help to see func");
+
     let mut app = FileFinder::new();
+
     app.set_extension(&mut my_readline)?;
 
     execute!(stdout(), terminal::Clear(terminal::ClearType::All))?;
@@ -28,6 +32,7 @@ struct FileFinder {
     extention: String,
     searchword: String,
 }
+
 impl FileFinder {
     fn new() -> Self {
         FileFinder {
@@ -36,26 +41,13 @@ impl FileFinder {
             searchword: "".to_string(),
         }
     }
+
     fn search(&mut self) -> Result<()> {
         let mut output_buffer = BufWriter::new(stdout());
-        let items = filefinder::finder(".", &self.searchword, &self.extention);
-        self.stack_vec = Vec::new();
-        // 該当項目のcounterとスタックする上限
-        let mut counter = 0;
-        let stack_limit = 100;
-
-        // 該当する項目のカウントと要素のスタック
-        for item in items.filter(|p| p.is_ok()).map(|p| p.unwrap()) {
-            if filefinder::andsearch(&item, &self.searchword) {
-                if self.stack_vec.len() < stack_limit {
-                    self.stack_vec.push(item);
-                }
-                counter += 1;
-            }
-        }
+        self.stack_vec = filefinder::item_search(".", &self.extention, &self.searchword);
 
         // 表示する項目数の制限
-        let limitnum = self.stack_vec.len().min(stack_limit);
+        let limitnum = self.stack_vec.len().min(DISPLAY_LIMIT);
 
         for it in self.stack_vec[..limitnum].iter() {
             writeln!(output_buffer, "{:?}", it)?;
@@ -64,7 +56,9 @@ impl FileFinder {
         writeln!(
             output_buffer,
             "extention: {},word: {} => {} hits",
-            self.extention, self.searchword, counter
+            self.extention,
+            self.searchword,
+            self.stack_vec.len()
         )?;
 
         output_buffer.flush()?;
@@ -75,7 +69,7 @@ impl FileFinder {
         loop {
             match my_readline.readline("extension >>") {
                 Ok(rline) => {
-                    if rline == "" {
+                    if rline.is_empty() {
                         println!("please input search word... '*' is wildcard.");
                         continue;
                     } else {
@@ -141,8 +135,9 @@ impl FileFinder {
     }
 
     fn manage_token(&mut self, my_readline: &mut Editor<()>) -> Result<()> {
+        let mut output_buffer = BufWriter::new(stdout());
+
         loop {
-            let mut output_buffer = BufWriter::new(stdout());
             writeln!(
                 output_buffer,
                 "@ext << change extention, @open << open file, @q or @quit << exit"
